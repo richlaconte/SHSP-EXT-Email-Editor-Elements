f0d014b9f7cfcf4b908450d20dc23142b5ed7160
Richard LaConte <38797549+pakjones@users.noreply.github.com>
2020-02-20 18:19:13 -0500

    Fixed dragndrop layout

--- content.js
+++ content.js
@@ -1,4 +1,59 @@
+const styles = `
+<style>.email-drag-item {height: 94px;}</style>
+`
 
+const spacerEditorHTML = `
+<ul class="editor-spacer">
+    <li class="note">
+        <h3>Spacer</h3>
+    </li>
+    <li class="header">
+        <h4>Styles</h4>
+    </li>
+    <li>
+        <label>Height</label>
+        <div class="editor-input small-values">
+            <input class="editor-onkeydown editor-onchange" button-setting="verticalPadding" type="text" value="12" button-value="12">
+        </div>
+    </li>
+    <li class="note">
+        <h3><i class="icon-margin"></i>  Margin</h3>
+    </li>
+    <li>
+        <label>Top</label>
+        <div class="editor-input small-values">
+            <input class="editor-onkeydown editor-onchange" button-setting="topSpacing" type="text" value="1">
+        </div>
+    </li>
+    <li>
+        <label>Bottom</label>
+        <div class="editor-input small-values">
+            <input class="editor-onkeydown editor-onchange" button-setting="bottomSpacing" type="text" value="1">
+        </div>
+    </li>
+    <li class="note">
+        <h3><i class="icon-border"></i> Border</h3>
+    </li>
+    <li>
+        <label>Color</label>
+        <div class="button-option colors editor-input">
+            <input type="text" maxlength="6" size="6" class="content-block-pickable" button-setting="borderColor" value="rgb(132, 195, 64)" style="background: rgb(132, 195, 64);">
+        </div>
+    </li>
+    <li>
+        <label>Width</label>
+        <div class="editor-input small-values">
+            <input class="editor-onkeydown editor-onchange" button-setting="borderWidth" type="text" value="1" button-value="1">
+        </div>
+    </li>
+    <li>
+        <label>Corners</label>
+        <div class="editor-input small-values">
+            <input class="editor-onkeydown editor-onchange" button-setting="borderRadius" type="text" value="3" button-value="3">
+        </div>
+    </li>
+</ul>
+`
 
 const hitSave = () => {
     let saveBtn = document.getElementById("saveEmail");
@@ -7,9 +62,16 @@ const hitSave = () => {
 
 
 const addDragAndDrops = () => {
+    let body = document.getElementsByTagName("body")[0];
+    let styleTag = document.createElement("STYLE");
+    body.appendChild(styleTag);
+    styleTag.outerHTML = styles;
+
     addOption("Divider", "<hr>");
     addOption("Spacer", "<br>");
     addOption("Social", "<h1>Social</h1>");
+
+    addSideBar();
 
     hitSave();
 }
@@ -60,6 +122,20 @@ const addOption = (name, htmlToAdd) => {
     let script = document.createElement("SCRIPT");
     body.appendChild(script);
     script.outerHTML = html;
+}
+
+let sideBar;
+let buttonEditor;
+let spacerEditor;
+
+const addSideBar = () => {
+  sideBar = document.getElementsByClassName("bc-settings")[0];
+  buttonEditor = document.getElementsByClassName("editor-button")[0];
+  spacerEditor = document.createElement("UL");
+  sideBar.appendChild(spacerEditor);
+  spacerEditor.outerHTML = spacerEditorHTML;
+
+  buttonEditor.style.display = "none";
 }
 
 addDragAndDrops();
